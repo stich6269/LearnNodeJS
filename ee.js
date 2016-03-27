0525028d90c87c0ed9877cc527839029f4892e37
stich6269 <stich6269@mail.ru>
2016-03-27 17:08:46 +0300

    - investigate event emitter

---
+++ ee.js
@@ -0,0 +1,49 @@
+/*var EventEmitter = require('events').EventEmitter;
+
+ var server = new EventEmitter;
+
+ server.on('request', function(request){
+ request.approved = true;
+ });
+
+ server.on('request', function(request){
+ console.log(request);
+ });
+
+ server.emit('request', {from: 'client'});
+ server. emit('request', {from: 'another client'});*/
+
+//console.log(server.listeners('request') );
+
+var EventEmitter = require('events').EventEmitter,
+    db = new EventEmitter;
+
+function Request(){
+    var self = this;
+    this.bigData = new Array(1e6).join('*');
+    
+    this.send = function(data){
+        console.log(data);
+    };
+    
+    this.onError = function(){
+        self.send('Sorry, we have some error');
+    };
+
+    function data (info){
+        self.send('info');
+    }
+
+    db.on('data', data);
+
+    this.end = function(){
+        db.removeListener('data', data)
+    };
+}
+
+setInterval(function(){
+    var request = new Request();
+    request.end();
+    console.log(process.memoryUsage().heapUsed);
+    //console.log(db);
+}, 200);
